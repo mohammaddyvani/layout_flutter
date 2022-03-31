181c40a99b6edf919c48be186e094fc9862df546
FlutLab.io <>
2022-03-31 14:13:38 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.red,
       ),
       home: MyHomePage(title: 'Gallery'),
+      showDebugCheckedModeBanner: false,
     );
   }
 }
